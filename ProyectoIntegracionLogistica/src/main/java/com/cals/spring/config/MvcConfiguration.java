a90c59d9a171794086ac71f2010642b2f4d1af7d
scordova <scordova@PC-TIC-T-25-109.cmch.com.pe>
2019-12-04 16:17:22 -0500

    Final 1

--- ProyectoIntegracionLogistica/src/main/java/com/cals/spring/config/MvcConfiguration.java
+++ ProyectoIntegracionLogistica/src/main/java/com/cals/spring/config/MvcConfiguration.java
@@ -12,8 +12,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import com.cals.spring.dao.ContactDAO;
-import com.cals.spring.dao.ContactDAOImpl;
 import com.cals.spring.dao.ProductoDAO;
 import com.cals.spring.dao.ProductoDAOImpl;
 
@@ -55,34 +53,4 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	}
 	
 	
-	/*
-	@Bean
-	public ViewResolver getViewResolver(){
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
-		
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
-
-	@Bean
-	public DataSource getDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/integracion");
-		dataSource.setUsername("root");
-		dataSource.setPassword("");
-		
-		return dataSource;
-	}
-		
-	@Bean
-	public ContactDAO getContactDAO() {
-		return new ContactDAOImpl(getDataSource());
-	}
-	*/
 }
